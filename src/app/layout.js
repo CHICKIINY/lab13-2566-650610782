143e876c660a9c2e224cf651246c295cd8e40472
CHICKIINY <bb.bright36389@gmail.com>
2023-08-09 03:39:06 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Panithan Boonamapi"
+            fullName="Panithan Boonmapi"
             studentId="650610782"
           />
         </div>
